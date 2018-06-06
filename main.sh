9593ee8df97488674d7b2c5a89382fc8eb3ad143
aivantg <aivant.developer@gmail.com>
2018-06-05 19:52:37 -0500

    init

---
+++ main.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+function export-dev-path(){
+  while IFS=, read -r -a array
+  do
+    export BASEDEVPATH="${array[0]}"
+  done < "/Users/aivant/.project_switcher/base_path.txt"
+}
+
+export-dev-path
+
+function update-dev-path(){
+  echo $1 > tmp
+  mv tmp /Users/aivant/.project_switcher/base_path.txt
+  export-dev-path
+}
+
+function dev-path(){
+  echo "Current Dev Path: $BASEDEVPATH"
+  echo "Change with 'update-dev-path <new path>'"
+}
+
+function mkalias(){
+  echo "$1,$2" >> "/Users/aivant/.project_switcher/project_aliases.txt"
+}
+
+function lsalias(){
+  echo -e "\n---Current Aliases---\n"
+  while IFS=, read -r -a array
+  do
+    echo "${array[0]} --> ${array[1]} "
+  done < "/Users/aivant/.project_switcher/project_aliases.txt"
+  echo -e "\nAdd more with 'mkalias <shortname> <path>'\nRemove with 'rmalias <shortname>'\n"
+}
+
+function rmalias(){
+  echostring="Aliases Removed:"
+  while IFS=, read -r -a array
+  do
+    if [ "${array[0]}" != "$1" ]; then
+      echo "${array[0]},${array[1]}"
+    else
+      echostring="$echostring\n${array[0]} --> ${array[1]}"
+    fi
+
+  done < /Users/aivant/.project_switcher/project_aliases.txt > temp
+  mv temp /Users/aivant/.project_switcher/project_aliases.txt
+  if [ "$echostring" != "Aliases Removed:" ]; then
+    echo -e "$echostring"
+  else
+    echo -e "No aliases found matching pattern: '$1'"
+  fi
+}
+
+
+function dev(){
+  # Read aliases from alias file
+  declare -A PROJALIASES
+  while IFS=, read -r -a array
+  do
+    PROJALIASES["${array[0]}"]="${array[1]}"
+  done < "/Users/aivant/.project_switcher/project_aliases.txt"
+
+  PROJSTRING="$BASEDEVPATH"
+  for PARAM in "$@"
+  do
+    FOLDER=${PROJALIASES[$PARAM]}
+    if [ "$FOLDER" = "" ]; then
+      CHECKINGPREFIXES=false
+      PROJSTRING="$PROJSTRING/$PARAM"
+    else
+      PROJSTRING="$PROJSTRING/$FOLDER"
+    fi
+  done
+  cd $PROJSTRING
+}
